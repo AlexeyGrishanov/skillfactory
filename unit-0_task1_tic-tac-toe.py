5220ce3dbd0e892e256bdc288e8871f303d7d5c8
Alexey Grishanov <agrishanov@tula.co>
2021-05-03 16:39:26 +0300

    Refactoring arter mentor's feedback

--- unit-0_task1_tic-tac-toe.py
+++ unit-0_task1_tic-tac-toe.py
@@ -1,8 +1,11 @@
 n = 3
 def_value = '-'
+# Генерируем поле
+board = [[def_value for j in range(n)] for i in range(n)]
 
 
 def draw(arr):
+    """Функция выводит поле с ходом игры после каждого шага"""
     print('{:2}'.format(''), ''.join(['{:4}'.format(str(i)) for i, row in enumerate(arr)]))
     print('\n'.join(
         ['{:2} {}'.format(str(i), ''.join(['{:4}'.format(item) for item in row])) for i, row in enumerate(arr)]))
@@ -10,11 +13,18 @@ def draw(arr):
 
 
 def play_game(token):
+    """Функция для ввода хода игрока"""
     value = input('Ход игрока {} (введите через пробел номер строки и номер колонки): '.format(token))
-    values = list(map(int, value.strip().split(' ')))
+
+    try:
+        values = list(map(int, value.strip().split(' ')))
+    except ValueError:
+        print('Некорректный ввод. Введите два числа через пробел')
+        return False
 
     if len(values) != 2:
         print('Некорректный ввод. Введите два числа через пробел')
+        return False
 
     row = values[0]
     col = values[1]
@@ -37,51 +47,64 @@ def play_game(token):
 
 
 def check_winner(arr, token):
+    """Функция проверки окончания игры и определения победителя"""
     columns = list(zip(*arr))
 
     for i, row in enumerate(arr):
+        # проверка по горизонтали
         if row.count(token) == n:
             return True
+        # проверка по веритикали
         elif columns[i].count(token) == n:
             return True
+        # проверка по диогонали слева направо
         elif [arr[i][i] for i in range(len(arr))].count(token) == n:
             return True
+        # проверка по диогонали справа налево
         elif [arr[i][-i - 1] for i in range(len(arr))].count(token) == n:
             return True
 
     return False
 
 
-board = [[def_value for j in range(n)] for i in range(n)]
+def start():
+    """Функция запуска игры"""
+
+    winner = None
+    player1 = 'X'
+    player2 = 'O'
+    current_player = player2
+    step = 0
+
+    while winner is None:
+        step += 1
+        current_player = player1 if current_player == player2 else player2
+
+        if not play_game(current_player):
+            current_player = player1 if current_player == player2 else player2
+            continue
+
+        draw(board)
+
+        if check_winner(board, current_player):
+            winner = current_player
+            break
+
+        if step >= n ** 2:
+            break
+
+    if winner is not None:
+        print('Game over. Победитель: игрок {}'.format(winner).upper())
+    else:
+        print('Game over. Ничья'.upper())
+
 
 print()
 print('Игра крестики-нолики'.upper())
 print()
+print('Правила игры:'.upper())
+print('Игроки по очереди ставят на свободные клетки поля знаки (один всегда крестики, другой всегда нолики).')
+print('Первый, выстроивший в ряд своих фигуры по вертикали, горизонтали или диагонали, выигрывает.')
+print()
 
-winner = None
-player1 = 'X'
-player2 = 'O'
-current_player = player2
-step = 0
-
-while winner is None:
-    step += 1
-    current_player = player1 if current_player == player2 else player2
-
-    if not play_game(current_player):
-        current_player = player1 if current_player == player2 else player2
-        continue
-
-    draw(board)
-
-    if check_winner(board, current_player):
-        winner = current_player
-        break
-
-    if step >= n**2:
-        break
-
-if winner is not None:
-    print('Game over. Победитель: игрок {}'.format(winner).upper())
-else:
-    print('Game over. Ничья'.upper())
+start()
